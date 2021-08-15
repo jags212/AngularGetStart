419ed31c680b2341c72be2b4f666af7cfadeb4dc
Jagadish Mohanty <j.or.mohanty@gmail.com>
2021-08-15 22:59:06 +0530

    Initial Commit

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,41 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+
+export class ProductListComponent {
+    pageTitle: string = 'Product List';
+    imageWidth: number=50;
+    imageMargin:number=2;
+    showImage: boolean=false;
+    listFilter: string = 'cart';
+
+    products: any[] = [
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+          },
+          {
+            "productId": 8,
+            "productName": "Saw",
+            "productCode": "TBX-0022",
+            "releaseDate": "May 15, 2021",
+            "description": "15-inch steel blade hand saw",
+            "price": 11.55,
+            "starRating": 3.7,
+            "imageUrl": "assets/images/saw.png"
+          }
+    ];
+
+    toggleImage(): void {
+       this.showImage = !this.showImage;       
+    }
+}
